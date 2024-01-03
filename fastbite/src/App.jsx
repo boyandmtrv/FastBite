6c6242ae6bd9439f2de9553813e769396cb9deb0
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-03 11:52:07 +0200

    testing queries

--- fastbite/src/App.jsx
+++ fastbite/src/App.jsx
@@ -1,18 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 import { appId, appKey } from './api/key';
 import Axios from 'axios';
 
 function App() {
 
-    const url = `https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}&from=0&to=3&calories=591-722&health=alcohol-free`;
+    const [query, setQuery] = useState('');
+
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&from=0&to=3&calories=591-722&health=alcohol-free`;
 
     async function getRecipes() {
         const result = await Axios.get(url);
         console.log(result.data);
     };
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        getRecipes();
+    };
+
     return (
-        <h1 onClick={getRecipes}>Hi there</h1>
+        <div className='App'>
+            <p onClick={getRecipes}>Test heading</p>
+            <form className='searchForm' onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder='Enter food'
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <input
+                    type="submit"
+                    value="Search"
+                />
+            </form>
+        </div>
     )
 }
 
