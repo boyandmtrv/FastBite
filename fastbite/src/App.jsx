4a746e701664d0ecdb394ec5309bbc1b539ef93b
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-03 14:51:31 +0200

    Applied react-router-dom to App.jsx

--- fastbite/src/App.jsx
+++ fastbite/src/App.jsx
@@ -4,27 +4,46 @@ import { appId, appKey } from './api/key';
 import AllRecipes from './components/recipes/AllRecipes';
 import Search from './components/search/Search';
 import { getRecipes } from './api/api';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Details from './components/details/Details';
+import Home from './components/home/Home';
+
 
 function App() {
 
     const [query, setQuery] = useState('');
     const [recipes, setRecipes] = useState([]);
 
+    const navigate = useNavigate();
+
+
     const onSubmit = (e) => {
         e.preventDefault();
-        getRecipes(query, appId, appKey, setRecipes);
+        try {
+            getRecipes(query, appId, appKey, setRecipes);
+            navigate('/allRecipes');
+        } catch (error) {
+            console.error('Error fetching recipes:', error);
+        }
     };
 
     return (
         <div className='App'>
-            <p>Test heading</p>
             <Search
                 query={query}
                 setQuery={setQuery}
                 onSubmit={onSubmit}
             />
-            <AllRecipes recipes={recipes} />
-        </div>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/allRecipes' element={
+                    <AllRecipes recipes={recipes} />}>
+                </Route>
+                <Route path='/details/:recipeId' element={<Details recipes={recipes} />} >
+                </Route>
+
+            </Routes>
+        </div >
     )
 }
 
