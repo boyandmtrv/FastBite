475bceddf7009bcd68f21cda04345ed95a41b4a4
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-03 11:33:29 +0200

    fetching data

--- fastbite/src/App.jsx
+++ fastbite/src/App.jsx
@@ -1,8 +1,18 @@
-import './App.css'
+import './App.css';
+import { appId, appKey } from './api/key';
+import Axios from 'axios';
 
 function App() {
+
+    const url = `https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}&from=0&to=3&calories=591-722&health=alcohol-free`;
+
+    async function getRecipes() {
+        const result = await Axios.get(url);
+        console.log(result.data);
+    };
+
     return (
-        <h1>Hi</h1>
+        <h1 onClick={getRecipes}>Hi there</h1>
     )
 }
 
---
+++ fastbite/src/api/key.js
@@ -0,0 +1,2 @@
+export const appId = 'd96ae6ec';
+export const appKey = '6f62eb038361098bb9b74101c43ee329';
