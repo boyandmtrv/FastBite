3d310def33783f35bbb55640c5ca5228e45a92b9
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-03 14:53:35 +0200

    Added details button

--- fastbite/src/components/recipes/EachRecipes.jsx
+++ fastbite/src/components/recipes/EachRecipes.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 const EachRecipes = ({
     recipe
 }) => {
+    const navigate = useNavigate();
+
+    const handleDetailsClick = () => {
+        navigate(`/details/${recipe.recipe['label']}`);
+    };
+
+    const product = recipe.recipe;
 
     return (
         <div className="">
-            <img className="recipiesImg" src={recipe.recipe['image']} />
-            <p className="recipiesTitle">{recipe.recipe['label']}</p>
-            <p className="uri">{recipe.recipe['url']}</p>
+            <img className="recipiesImg" src={product['image']} />
+            <p className="recipiesTitle">{product['label']}</p>
+            <button onClick={handleDetailsClick}>Details</button>
         </div>
     );
 };
