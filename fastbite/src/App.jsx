5107224551657bac0e7a5e012e9fbb63030ef424
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-04 14:29:34 +0200

    Added loader state

--- fastbite/src/App.jsx
+++ fastbite/src/App.jsx
@@ -10,22 +10,29 @@ import Home from './components/home/Home';
 import AllRecipes from './components/recipes/AllRecipes';
 import Search from './components/search/Search';
 import Details from './components/details/Details';
+import Loading from './components/loading/Loading';
 // import NoTimeSearch from './components/search/NoTimeSearch';
 
 function App() {
 
     const [query, setQuery] = useState('');
     const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(false);
+
 
     const navigate = useNavigate();
 
 
     const onSubmit = async (query, maxTime) => {
         try {
+            setLoading(true);
             await getRecipes(query, appId, appKey, setRecipes, parseInt(maxTime));
             navigate('/allRecipes');
         } catch (error) {
             console.error('Error fetching recipes:', error);
+        }
+        finally {
+            setLoading(false);
         }
     };
 
@@ -37,15 +44,15 @@ function App() {
                 setQuery={setQuery}
                 onSubmit={onSubmit}
             />
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/allRecipes' element={
-                    <AllRecipes recipes={recipes} />}>
-                </Route>
-                <Route path='/details/:recipeId' element={<Details recipes={recipes} />} >
-                </Route>
-
-            </Routes>
+             {loading ? (
+                <Loading />
+            ) : (
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/allRecipes' element={<AllRecipes recipes={recipes} />}></Route>
+                    <Route path='/details/:recipeId' element={<Details recipes={recipes} />} ></Route>
+                </Routes>
+            )}
         </div >
     )
 }
