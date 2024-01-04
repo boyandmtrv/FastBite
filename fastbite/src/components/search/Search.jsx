8e26c616fb6bcce2ca7039207825adc00fe84e14
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-04 09:53:49 +0200

    Search Component updated - added maxTime as a value

--- fastbite/src/components/search/Search.jsx
+++ fastbite/src/components/search/Search.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react";
+
 const Search = ({
     query,
     setQuery,
     onSubmit
 }) => {
+
+    const [maxTime, setMaxTime] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        
+        onSubmit(query, maxTime);
+    };
+
     return (
         <div className="search">
-            <form className="searchForm" onSubmit={onSubmit}>
+            <form className="searchForm" onSubmit={handleSearch}>
                 <input
                     type="text"
                     placeholder='Enter food'
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                 />
+                 <input
+                    type="number"
+                    placeholder='Time you have'
+                    value={maxTime}
+                    onChange={(e) => setMaxTime(e.target.value)}
+                />
                 <input
                     type="submit"
                     value="Search"
                 />
+                
             </form>
         </div>
     )
