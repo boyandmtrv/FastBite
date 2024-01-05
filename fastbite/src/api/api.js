9ede7b0df880e96bb81c4c6a69f935d771dd8ed2
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-05 13:06:36 +0200

    Added cuisineType searching in the api

--- fastbite/src/api/api.js
+++ fastbite/src/api/api.js
@@ -1,33 +1,29 @@
 import Axios from 'axios';
 
-export const getRecipes = async (query, appId, appKey, setRecipes, maxTotalTime) => {
+export const getRecipes = async (query, appId, appKey, setRecipes, maxTotalTime, cuisineType) => {
     const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&from=0&to=100&calories=591-722&health=alcohol-free`;
 
     try {
         const result = await Axios.get(url);
 
         let filteredRecipes = result.data.hits;
-        
+
         if (maxTotalTime) {
-            filteredRecipes = result.data.hits.filter(recipe => {
+            filteredRecipes = result.data.hits.filter((recipe) => {
                 const totalTime = recipe.recipe.totalTime || 0;
-                console.log(`Recipe time ${totalTime}`);
-                console.log(`Recipe name ${recipe.recipe.label}`);
-                console.log(`Max: ${maxTotalTime}`);
                 return totalTime > 0 && totalTime <= maxTotalTime;
             });
         }
 
-        // const filteredRecipes = result.data.hits.filter(recipe => {
-        //     const totalTime = recipe.recipe.totalTime || 0; 
-        //     console.log(`Recipe time ${totalTime}`);
-        //     console.log(`Recipe name ${recipe.recipe.label}`);
-        //     console.log(`Max: ${maxTotalTime}`);
-        //     return totalTime > 0 && totalTime <= maxTotalTime;
-        // });
-        console.log(filteredRecipes);
+        if (cuisineType) {
+            filteredRecipes = filteredRecipes.filter((recipe) => {
+                const cuisines = recipe.recipe.cuisineType || [];
+                return cuisines.includes(cuisineType);
+            });
+        }
 
         setRecipes(filteredRecipes);
+
         console.log(result.data);
     } catch (error) {
         console.error('Error fetching recipes:', error);
