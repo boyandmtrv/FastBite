52a5a1a37d7b42ac10b8abc4d9f248bb033359ac
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-05 13:07:05 +0200

    Created new component based on time search

---
+++ fastbite/src/components/search/TimeSearch.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const TimeSearch = ({
+    query,
+    setQuery,
+    onSubmit
+}) => {
+
+    const [maxTime, setMaxTime] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        
+        onSubmit(query, maxTime);
+    };
+
+    return (
+        <div className="search">
+            <form className="searchForm" onSubmit={handleSearch}>
+                <input
+                    type="text"
+                    placeholder='Enter food'
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                 <input
+                    type="number"
+                    placeholder='Time you have'
+                    value={maxTime}
+                    onChange={(e) => setMaxTime(e.target.value)}
+                />
+                <input
+                    type="submit"
+                    value="Search"
+                />
+                
+            </form>
+        </div>
+    )
+};
+
+export default TimeSearch;
