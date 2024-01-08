a08e9ea6e2c9d930353a6931c7dcdf47b3ea5a8a
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-08 14:40:00 +0200

    Re-styling home component

--- fastbite/src/components/home/Home.jsx
+++ fastbite/src/components/home/Home.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import './Home.css';
-import { GiHamburger, GiFriedEggs, GiMeat } from 'react-icons/gi';
+import { PiHamburger } from "react-icons/pi";
+import { LuBeef, LuSalad } from "react-icons/lu";
+import { MdOutlineFastfood } from "react-icons/md";
+import { BsCake } from "react-icons/bs";
+import { LiaBreadSliceSolid } from "react-icons/lia";
 
 const Home = () => {
     const [timer, setTimer] = useState(4);
@@ -20,39 +24,39 @@ const Home = () => {
     }, [timer]);
 
     return (
-        <div className={`flex items-center justify-center h-full`}>
+        <div className={`flex items-center justify-center h-full bg-[#140014]`}>
             <div className={`pacmanContainer ${isVisible ? 'fade-out' : ''}`}>
                 {isVisible && (
                     <div className="pacmanTotal flex items-center overflow-hidden h-[50vh] w-full">
                         <div className="pacman relative bg-transparent w-[300px]"></div>
-                        <div className="dot border-[10px] border-white rounded-[50%] mt-[-150px] ml-[150px]"></div>
-                        <div className="path flex justify-around w-full pl-[10px] text-[80px]">
-                            <GiHamburger />
+                        <div className="dot border-[10px] border-black rounded-[50%] mt-[-150px] ml-[150px]"></div>
+                        <div className="path flex justify-around w-full pl-[10px] text-[80px] text-[#1414f0]">
+                            <PiHamburger />
                         </div>
-                        <div className="path flex justify-around w-full pl-[10px] text-[80px]">
-                            <GiFriedEggs />
+                        <div className="path flex justify-around w-full pl-[10px] text-[80px] text-[#1414f0]">
+                            <LuBeef />
                         </div>
-                        <div className="path flex justify-around w-full pl-[10px] text-[80px]">
-                            <GiMeat />
+                        <div className="path flex justify-around w-full pl-[10px] text-[80px] text-[#1414f0]">
+                            <LuSalad />
                         </div>
-                        <div className="path flex justify-around w-full pl-[10px] text-[80px]">
-                            <GiHamburger />
+                        <div className="path flex justify-around w-full pl-[10px] text-[80px] text-[#1414f0]">
+                            <MdOutlineFastfood />
                         </div>
-                        <div className="path flex justify-around w-full pl-[10px] text-[80px]">
-                            <GiFriedEggs />
+                        <div className="path flex justify-around w-full pl-[10px] text-[80px] text-[#1414f0]">
+                            <BsCake />
                         </div>
-                        <div className="path flex justify-around w-full pl-[10px] text-[80px]">
-                            <GiMeat />
+                        <div className="path flex justify-around w-full pl-[10px] text-[80px] text-[#1414f0]">
+                            <LiaBreadSliceSolid />
                         </div>
-                        <div className="path flex justify-around w-full pl-[10px] text-[80px]">
-                            <GiHamburger />
+                        <div className="path flex justify-around w-full pl-[10px] text-[80px] text-[#1414f0]">
+                            <PiHamburger />
                         </div>
                     </div>
                 )}
             </div>
             {!isVisible && (
-                <div className={`check fade-in`}>
-                    Welcome to hngrPac
+                <div className={`homeContainer fade-in`}>
+                    <h1 className='text-white'>hngrPac</h1>
                 </div>
             )}
         </div>
