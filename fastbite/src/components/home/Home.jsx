f334fcd82d2eb5221a260a8b2e70e59d3c14f324
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-08 12:39:45 +0200

    Re-structuring home component

--- fastbite/src/components/home/Home.jsx
+++ fastbite/src/components/home/Home.jsx
@@ -1,26 +1,59 @@
+import { useEffect, useState } from 'react';
 import './Home.css';
+import { GiHamburger, GiFriedEggs, GiMeat } from 'react-icons/gi';
 
 const Home = () => {
-    const letters = "hngrPac";
+    const [timer, setTimer] = useState(3);
+    const [isVisible, setIsVisible] = useState(true);
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setTimer((prevTimer) => prevTimer - 1);
+    //     }, 1000);
+
+    //     if (timer === 0) {
+    //         clearInterval(interval);
+    //         setIsVisible(false);
+    //     }
+
+    //     return () => clearInterval(interval);
+    // }, [timer]);
 
     return (
-        <div className="main">
-            <div className="center">
-                <div className="pacman-con">
-                    <div className="pacman"></div>
-                    <div className="mouth"></div>
-                    <div className="mouth"></div>
-                </div>
-                {letters.split("").map((letter, index) => (
-                    <div
-                        key={index}
-                        className="letters"
-                        style={{ '--gap': index }}
-                    >
-                        {letter}
+        <div className={`homeContainer`}>
+            <div className={`pacmanContainer ${isVisible ? 'fade-in' : ''}`}>
+                {isVisible && (
+                    <div className="pacmanTotal">
+                        <div className="pacman"></div>
+                        <div className="path">
+                            <GiHamburger />
+                        </div>
+                        <div className="path">
+                            <GiFriedEggs />
+                        </div>
+                        <div className="path">
+                            <GiMeat />
+                        </div>
+                        <div className="path">
+                            <GiHamburger />
+                        </div>
+                        <div className="path">
+                            <GiFriedEggs />
+                        </div>
+                        <div className="path">
+                            <GiMeat />
+                        </div>
+                        <div className="path">
+                            <GiHamburger />
+                        </div>
                     </div>
-                ))}
+                )}
             </div>
+            {!isVisible && (
+                <div className={`check fade-in`}>
+                    hi
+                </div>
+            )}
         </div>
     );
 };
