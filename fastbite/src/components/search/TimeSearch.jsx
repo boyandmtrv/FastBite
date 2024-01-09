28a22f68fce1523b99b2a2508ab73bb81675ec2c
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-09 14:58:17 +0200

    Started styling TimeSearch Component

--- fastbite/src/components/search/TimeSearch.jsx
+++ fastbite/src/components/search/TimeSearch.jsx
@@ -7,36 +7,61 @@ const TimeSearch = ({
 }) => {
 
     const [maxTime, setMaxTime] = useState('');
+    const [isVisible, setIsVisible] = useState(false);
+
 
     const handleSearch = (e) => {
         e.preventDefault();
-        
         onSubmit(query, maxTime);
+        setQuery('');
+        setMaxTime('');
+    };
+
+    const handleNext = () => {
+        setIsVisible(true);
     };
 
     return (
-        <div className="search bg-white w-full mt-[10%]">
-            <form className="searchForm" onSubmit={handleSearch}>
-                <input
-                    type="text"
-                    placeholder='Enter food'
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                />
-                 <input
-                    type="number"
-                    placeholder='Time you have'
-                    value={maxTime}
-                    onChange={(e) => setMaxTime(e.target.value)}
-                />
-                <input
-                    type="submit"
-                    value="Search"
-                />
-                
-            </form>
+        <div className="search">
+            <div className="bg-pacBg ">
+                <form className="searchForm backdrop-blur-md flex w-full h-screen flex-row items-center justify-center" onSubmit={handleSearch}>
+                    {!isVisible && (
+                        <>
+                            <input
+                                type="text"
+                                className=" w-[40%] h-16 pl-5 border border-black rounded-3xl text-2xl"
+                                placeholder='Enter food'
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                            />
+                            <button
+                                type="submit"
+                                className=" bg-slate-500 ml-5 w-36 h-16 border rounded-3xl"
+                                onClick={handleNext}>
+                                Next
+                            </button>
+                        </>
+                    )}
+                    {isVisible && (
+                        <>
+                            <input
+                                type="number"
+                                className=" w-[40%] h-16 pl-5 border border-black rounded-3xl text-2xl"
+                                placeholder='Time you have'
+                                value={maxTime}
+                                onChange={(e) => setMaxTime(e.target.value)}
+                            />
+                            <input
+                                type="submit"
+                                className=" bg-slate-500 ml-5 w-36 h-16 border rounded-3xl"
+                                value="Search"
+                            />
+                        </>
+                    )}
+                </form>
+            </div>
         </div>
-    )
+    );
 };
 
 export default TimeSearch;
