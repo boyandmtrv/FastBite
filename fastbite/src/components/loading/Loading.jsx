ade1ac2816cc0654e6838d481f9cd1e19a7f99ef
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-09 18:16:29 +0200

    Loader component finished for now

--- fastbite/src/components/loading/Loading.jsx
+++ fastbite/src/components/loading/Loading.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Loading.css'
 
 const Loading = () => {
+    
     return (
-        <div className='mt-[20%]' style={{ textAlign: 'center', paddingTop: '20px' }}>
-            Loading...
+        <div className='pacmanIconLoading bg-[#F9F5EA] w-full h-screen flex flex-col items-center justify-center'>
+            <span className='mt-[10%] ml-5'>
+                Checking...
+            </span>
         </div>
     );
 };
