d2a9192d7bd6b430bcb9470317fd13bd5b21ec55
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-10 12:45:44 +0200

    Minor style changes

--- fastbite/src/components/header/Header.jsx
+++ fastbite/src/components/header/Header.jsx
@@ -29,11 +29,11 @@ const Header = () => {
     };
 
     return (
-        <div className="w-full fixed top-0 left-0 shadow-md z-10 bg-white">
+        <div className="w-full fixed top-0 left-0 shadow-md z-10 bg-transparent">
             {!isVisible && (
                 <div className="fade-in md:flex items-center justify-center  py-4 md:px-10 px-7 mr-[5%]">
-                    <div className="font-bold text-2xl cursor-pointer flex items-center md:hidden">
-                        <Link className="text-[#fff]" to="/">
+                    <div className="font-cursive text-2xl cursor-pointer flex items-center md:hidden">
+                        <Link className="text-[#FFB897]" to="/">
                             hngrPac
                         </Link>
                     </div>
@@ -44,25 +44,25 @@ const Header = () => {
                             <ImPacman className='scale-y-[-1] rotate-90 transition-transform transform hover:scale-6' />
                         )}
                     </div>
-                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-400 ease-in ${openMenu ? 'top-[65px] opacity-100 bg-white ' : 'top-[-500px]'} md:opacity-100 opacity-0`}>
-                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "Search" ? 'font-cursive text-[2rem] text-[#f7d7c9] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
+                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-400 ease-in ${openMenu ? 'top-[65px] opacity-100 bg-[#F9F5EA] ' : 'top-[-500px]'} md:opacity-100 opacity-0`}>
+                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "Search" ? 'font-cursive text-[2rem] text-[#FFB897] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
                             <Link onClick={() => handleItemClick("Search")} to="/search">Search</Link>
                         </li>
-                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "SearchByCuisine" ? 'font-cursive text-[2rem] text-[#f7d7c9] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
+                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "SearchByCuisine" ? 'font-cursive text-[2rem] text-[#FFB897] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
                             <Link onClick={() => handleItemClick("SearchByCuisine")} to="/cuisineSearch">Search by Cuisine</Link>
                         </li>
 
-                        <li className={`header-heading md:my-0 my-7 cursor-pointer items-center lg:flex md:flex sm: hidden min-[320px]:hidden  ml-8 font-cursive ${selectedItem === "Home" ? 'font-cursive text-[2rem] text-[#f7d7c9] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
+                        <li className={`header-heading md:my-0 my-7 cursor-pointer items-center lg:flex md:flex sm: hidden min-[320px]:hidden  ml-8 font-cursive ${selectedItem === "Home" ? 'font-cursive text-[2rem] text-[#FFB897] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
                             <Link onClick={() => handleItemClick("Home")} to="/">Home</Link>
                         </li>
                       
-                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "SearchByTime" ? 'font-cursive text-[2rem] text-[#f7d7c9] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
+                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "SearchByTime" ? 'font-cursive text-[2rem] text-[#FFB897] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
                             <Link onClick={() => handleItemClick("SearchByTime")} to="/timeSearch">Search by Time</Link>
                         </li>
 
                         
-                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "Menu" ? 'font-cursive text-[2rem] text-[#f7d7c9] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
-                            <Link onClick={() => handleItemClick("Menu")} to="/menu">Menu</Link>
+                        <li className={`md:ml-8 text-xl md:my-0 my-7 ${selectedItem === "About" ? 'font-cursive text-[2rem] text-[#FFB897] duration-500' : 'text-[#545454] text-xl hover:text-[#f7d7c9] duration-500'}`}>
+                            <Link onClick={() => handleItemClick("About")} to="/about">About</Link>
                         </li>
                     </ul>
                 </div>
