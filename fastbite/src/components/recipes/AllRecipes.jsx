d64a6b227126cdf31cc1bb15a55966b814c7f9b4
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-10 14:58:38 +0200

    Started styling each and All compoennt

--- fastbite/src/components/recipes/AllRecipes.jsx
+++ fastbite/src/components/recipes/AllRecipes.jsx
@@ -1,19 +1,17 @@
 import EachRecipes from './EachRecipes';
 
-const AllRecipies = ({
-    recipes
-}) => {
-    return (
-        <div className='allRecipes'>
-        {recipes.length > 0 ? (
-            recipes.map((recipe, index) => (
-                <EachRecipes key={`${recipe.recipe.label}-${index}`} recipe={recipe} />
-            ))
-        ) : (
-            <p>No recipes available. Perform a search.</p>
-        )}
+const AllRecipies = ({ recipes }) => {
+  return (
+    <div className='allRecipes  flex flex-wrap items-center justify-center lg:mt-[3%] md:mt-[6%] sm:mt-[8%] min-[320px]:mt-[13%] bg-[#F9F5EA]'>
+      {recipes.length > 0 ? (
+        recipes.map((recipe, index) => (
+          <EachRecipes key={`${recipe.recipe.label}-${index}`} recipe={recipe} />
+        ))
+      ) : (
+        <p className=''>No recipes available. Perform a search.</p>
+      )}
     </div>
-    )
+  );
 };
 
 export default AllRecipies;
--- fastbite/src/components/recipes/EachRecipes.jsx
+++ fastbite/src/components/recipes/EachRecipes.jsx
@@ -12,12 +12,23 @@ const EachRecipes = ({
     const product = recipe.recipe;
 
     return (
-        <div className="">
-            <img className="recipiesImg" src={product['image']} />
-            <p className="recipiesTitle">{product['label']}</p>
-            <button onClick={handleDetailsClick}>Details</button>
+        <div className="m-9 flex flex-col items-center justify-center w-[400px] h-[400px]">
+            <div className='group rounded-2xl mt-5'>
+                <p style={{ WebkitTextFillColor: 'transparent', WebkitTextStrokeWidth: '.3px' }} className="recipiesTitle text-7xl leading-[4rem] line-clamp-3 lg:w-[400px] md:w-[400px] sm:w-[400px]  min-[320px]:w-[320px] lg:h-[400px] md:h-[400px] sm:h-[400px] min-[320px]:h-[320px] pl-5 text-white absolute pt-[80px] bg-[#000000c5] rounded-2xl opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+                    {product['label']}
+                </p>
+        
+                <button className=' bg-[#00000025] border-white hover:bg-[#f7d7c9] text-xl w-36 h-12  flex flex-col items-center justify-center text-center absolute lg:mt-[17%] lg:ml-[12%] rounded-3xl opacity-0 text-white hover:text-black transition-opacity duration-500 group-hover:opacity-100' onClick={handleDetailsClick}>Details</button>
+
+
+                <img className="recipiesImg lg:w-[400px] lg:h-[400px] md:w-[400px] md:h-[400px] sm:w-[400px] sm:h-[400px] min-[320px]:w-[320px] min-320px:h-[320px] rounded-2xl" src={product['image']} />
+
+
+            </div>
         </div>
     );
 };
 
 export default EachRecipes;
+
+//opacity-0 transition-opacity duration-500 group-hover:opacity-100 bg-[#ffffff86]
