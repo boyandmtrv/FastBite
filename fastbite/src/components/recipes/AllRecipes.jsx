50561119ea72170718d0a94066addfe5b4806d2d
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-11 14:21:34 +0200

    Minor changes in all recipes

--- fastbite/src/components/recipes/AllRecipes.jsx
+++ fastbite/src/components/recipes/AllRecipes.jsx
@@ -2,13 +2,15 @@ import EachRecipes from './EachRecipes';
 
 const AllRecipies = ({ recipes }) => {
   return (
-    <div className='allRecipes  flex flex-wrap items-center justify-center lg:mt-[3%] md:mt-[6%] sm:mt-[8%] min-[320px]:mt-[13%] bg-[#F9F5EA]'>
+    <div className='allRecipes w-full flex flex-wrap items-center justify-center  bg-[#F9F5EA]'>
       {recipes.length > 0 ? (
         recipes.map((recipe, index) => (
           <EachRecipes key={`${recipe.recipe.label}-${index}`} recipe={recipe} />
         ))
       ) : (
-        <p className=''>No recipes available. Perform a search.</p>
+        <div className="h-screen w-full flex items-center justify-center">
+          <p className=''>No recipes available. Perform a search.</p>
+        </div>
       )}
     </div>
   );
