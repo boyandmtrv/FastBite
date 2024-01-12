dc4db9e151c0fa56e927a6b02122bd8ff410de1c
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2024-01-12 14:23:11 +0200

    Minor style changes

--- fastbite/src/components/recipes/AllRecipes.jsx
+++ fastbite/src/components/recipes/AllRecipes.jsx
@@ -1,4 +1,5 @@
 import EachRecipes from './EachRecipes';
+import { Link } from 'react-router-dom'
 
 const AllRecipies = ({ recipes }) => {
   return (
@@ -8,8 +9,9 @@ const AllRecipies = ({ recipes }) => {
           <EachRecipes key={`${recipe.recipe.label}-${index}`} recipe={recipe} />
         ))
       ) : (
-        <div className="h-screen w-full flex items-center justify-center">
-          <p className=''>No recipes available. Perform a search.</p>
+        <div className="h-screen w-full flex flex-col text-center items-center justify-center">
+          <p className='text-3xl'>No recipes available.</p>
+          <Link className='mt-5 border text-white bg-[#FFB897] hover:text-[#000] hover:bg-[#f7d7c9] duration-500 rounded-full w-56 h-12 items-center justify-center flex text-xl' to='/search'>Perform a search.</Link>
         </div>
       )}
     </div>
